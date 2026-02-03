db90efebb044a9de7aa432188eb28ecf96575873
Guido <guidoxenofonte@gmail.com>
2026-02-03 10:29:22 -0300

    move TAGS enum from lexer to globals

---
+++ src/globals.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+
+class TAGS(Enum):
+    IDENTIFIER = 256
+    INTEGER = 257
+    REAL = 258
+    BOOL = 259
+    VOID = 260
--- src/lexer.py
+++ src/lexer.py
@@ -1,14 +1,3 @@
-from enum import Enum
-
-
-class TAGS(Enum):
-    IDENTIFIER = 256
-    INTEGER = 257
-    REAL = 258
-    BOOL = 259
-    VOID = 260
-
-
 class Token:
     def __init__(self, tokenTag: str, tokenValue: any):
         self.tag: str = tokenTag
