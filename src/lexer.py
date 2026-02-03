df22cc31d7f763c958d38590ab7b8c7639a8c2dc
Guido <guidoxenofonte@gmail.com>
2026-02-03 17:17:30 -0300

    add peek and advance_position functions to lexer, better init function
    Mudanças na função init para melhorar legibilidade.
    
    Adição das funções peek para verificar o próximo caractere sem avançar a
    posição e advance_position para verificar o próximo caractere avançando
    a posição atual.

--- src/lexer.py
+++ src/lexer.py
@@ -5,18 +5,20 @@ class Token:
     def __init__(self, tokenTag: str, tokenValue: any):
         self.tag: str = tokenTag
         self.value: any = tokenValue
-    
+
     def __str__(self):
         return f"<{self.tag}, {self.value}>"
 
 
 class Lexer:
     def __init__(self, file: str):
-        self.token_table: dict = {}
-        self._init_reserved_table()
+        self._source_code = file.read()
 
-        for item in self._reserved_table.values():
-            print(item)
+        self.token_table: dict = {}
+        self._current_position = 0
+        self._current_line = 1
+
+        self._init_reserved_table()
 
     def _init_reserved_table(self):
         # TODO: terminar tabela de palavras reservadas
@@ -29,5 +31,20 @@ class Lexer:
             "false": Token(TAGS.FALSE, "false"),
             "print": Token(TAGS.PRINT, "print"),
         }
+
+    def peek(self) -> str:
+        if self._current_position < len(self._source_code):
+            return self._source_code[self._current_position]
+        return ''
     
+    def _advance_position(self) -> None:
+        char: str = self.peek()
+        
+        if char != '':
+            self._current_position += 1
+        
+        if char == '\n':
+            self._current_line += 1
+        
+        return char
 
