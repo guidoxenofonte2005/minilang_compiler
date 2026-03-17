2c7103efc541ee818cc9264a007d2225bae37d7a
Heitor <heitor.nb10@gmail.com>
2026-03-17 11:47:21 -0300

    adiciona as producoes restantes e corrige os FIRSTs não disjuntos entre <factor> e <function-call>

--- src/parser.py
+++ src/parser.py
@@ -30,9 +30,25 @@ class Parser:
         program := <statement_group>
         """
         return self.statements_group()
+    
+    def block(self):
+        """
+        ### REGRA:
+        <block> := "{" <statement_group> "}"
+        """
+        if not self.match_tag(ord("{")):
+            raise ParseError("Esperado '{'")
+
+        stmts = self.statements_group()
+
+        if not self.match_tag(ord("}")):
+            raise ParseError("Esperado '}'")
+
+        return Block(stmts)
 
     def statements_group(self) -> StatementSequence | None:
         """
+        ### REGRA:
         statement_group := <statement> <statement_group> | ε
         """
         if self.lookahead.tag in (
@@ -50,17 +66,6 @@ class Parser:
             return StatementSequence(statement, statement_group)
 
         return None
-        
-    # antes de atualizar remover caso necessario apos o pr
-    # def statements(self) -> StatementSequence | None:
-    #     """
-    #     ### REGRA:
-    #     statements := statement statements | ϵ
-    #     """
-    #     if self.lookahead.tag in (TAGS.IDENTIFIER.value, TAGS.PRINT.value):
-    #         statement = self.statement()
-    #         statement_group = self.statements()
-    #         return StatementSequence(statement, statement_group)
 
     def statement(self):
         if self.lookahead.tag == TAGS.VAR.value:
@@ -96,10 +101,10 @@ class Parser:
                 f"ERRO NA LINHA {self.get_current_line()}: statement inválido"
             )
 
-
     def function_decl(self):
         """
-        function-decl := "def" identifier "(" formal_param_optional ")" ":" type block
+        ### REGRA:
+        <function-decl> := "def" <identifier> "(" <formal_param_optional> ")" ":" <type> <block>
         """
 
         if not self.match_tag(TAGS.DEF.value):
@@ -107,12 +112,7 @@ class Parser:
                 f"ERRO NA LINHA {self.get_current_line()}: esperado 'def'"
             )
 
-        func_name_token = self.lookahead
-
-        if not self.match_tag(TAGS.IDENTIFIER.value):
-            raise ParseError(
-                f"ERRO NA LINHA {self.get_current_line()}: esperado identificador para nome da função"
-            )
+        identifier = self.identifier()
 
         if not self.match_tag(ord("(")):
             raise ParseError(
@@ -141,10 +141,11 @@ class Parser:
 
         block = self.block()
 
-        return FunctionDecl(func_name_token, params, return_type_token, block)
+        return FunctionDecl(identifier, params, return_type_token, block)
     
     def formal_param_optional(self):
         """
+        ### REGRA:
         <formal_param_optional> := <formal-param> <formal_param_list> | e
         """
         if self.lookahead.tag == TAGS.IDENTIFIER.value:
@@ -156,6 +157,7 @@ class Parser:
     
     def formal_param_list(self):
         """
+        ### REGRA:
         <formal_param_list> := "," <formal-param> <formal_param_list> | e
         """
         if self.lookahead.tag == ord(","):
@@ -168,6 +170,7 @@ class Parser:
         
     def formal_param(self):
         """
+        ### REGRA:
         <formal-param> := identifier ":" type
         """
         if self.lookahead.tag != TAGS.IDENTIFIER.value:
@@ -193,21 +196,124 @@ class Parser:
 
         return FormalParam(param_name_token, param_type_token)
     
-    # antes de atualizar remover caso necessario apos o pr
-    # def expression(self) -> Expression:
-    #     """
-    #     ### REGRA:
-    #     expression := simple-expression \{ relational-op simple-expression \}
-    #     """
-    #     expression = self.simple_expr()
+    def while_statement(self):
+        """
+        ### REGRA:
+        <while-statement> := "while" "(" <expression> ")" <block>
+        """
+        
+        if not self.match_tag(TAGS.WHILE.value):
+            raise ParseError("Esperado 'while'")
 
-    #     # TODO: fazer checagem de relational-op
-    #     while (self.lookahead.tag == TAGS.RELATIONAL_OP.value):
-    #         token = self.lookahead
-    #         self.match_tag(self.lookahead.tag)
-    #         right_hand_expr = self.simple_expr()
-    #         expression = LogicalExpr(token, )
-    #     return expression
+        if not self.match_tag(ord("(")):
+            raise ParseError("Esperado '('")
+
+        expr = self.expression()
+
+        if not self.match_tag(ord(")")):
+            raise ParseError("Esperado ')'")
+
+        block = self.block()
+
+        return WhileStatement(expr, block)
+
+    def if_statement(self):
+        """
+        ### REGRA:
+        <if-statement> := "if" "(" <expression> ")" <block> <else_block> 
+        """
+
+        if not self.match_tag(TAGS.IF.value):
+            raise ParseError("Esperado 'if'")
+
+        if not self.match_tag(ord("(")):
+            raise ParseError("Esperado '(' após if")
+
+        expr = self.expression()
+
+        if not self.match_tag(ord(")")):
+            raise ParseError("Esperado ')'")
+
+        block = self.block()
+        else_block = self.else_block()
+
+        return IfStatement(expr, block, else_block)
+
+
+    def else_block(self):
+        """
+        ### REGRA:
+        <else_block> := "else" <block> | e 
+        """
+        if self.lookahead.tag == TAGS.ELSE.value:
+            self.match_tag(TAGS.ELSE.value)
+            return self.block()
+        return None
+
+    def return_statement(self):
+        """
+        ### REGRA:
+        <return-statement> := "return" <expression> 
+        """
+        if not self.match_tag(TAGS.RETURN.value):
+            raise ParseError("Esperado 'return'")
+        expr = self.expression()
+        return ReturnStatement(expr)
+
+    def print_statement(self):
+        """
+        ### REGRA:
+        <print-statement> := "print" <expression> 
+        """
+        if not self.match_tag(TAGS.PRINT.value):
+            raise ParseError("Esperado 'print'")
+        expr = self.expression()
+        return PrintStatement(expr)
+
+    def variable_decl(self):
+        """
+        ### REGRA:
+        <variable-decl> := "var" <identifier> ":" <type> "=" <expression>
+        """
+
+        if not self.match_tag(TAGS.VAR.value):
+            raise ParseError("Esperado 'var'")
+
+        identifier = self.identifier()
+
+        if not self.match_tag(ord(":")):
+            raise ParseError("Esperado ':'")
+
+        if self.lookahead.tag != TAGS.TYPE.value:
+            raise ParseError("Esperado tipo")
+
+        type_token = self.lookahead
+        self.match_tag(TAGS.TYPE.value)
+
+        if not self.match_tag(ord("=")):
+            raise ParseError("Esperado '='")
+
+        expr = self.expression()
+
+        return VariableDecl(identifier, type_token, expr)
+
+    def assignment(self):
+        """
+        ### REGRA:
+        <assignment> := "set" <identifier> "=" <expression>
+        """
+
+        if not self.match_tag(TAGS.SET.value):
+            raise ParseError("Esperado 'set'")
+
+        identifier = self.identifier()
+
+        if not self.match_tag(ord("=")):
+            raise ParseError("Esperado '='")
+
+        expr = self.expression()
+
+        return Assignment(identifier, expr)
 
     def expression(self) -> Expression:
         """
@@ -217,6 +323,7 @@ class Parser:
         expression = self.simple_expression()
         relational_expr_list = self.relational_expression_list()
         return Expression(expression, relational_expr_list)
+    
     def relational_expression_list(self):
         """
         ### REGRA:
@@ -229,21 +336,7 @@ class Parser:
             relational_expr_list = self.relational_expression_list()
             return RelationalExpressionList(token, simple_expr, relational_expr_list)
         else:
-            return None   
-    # antes de atualizar remover caso necessario apos o pr
-    # def simple_expr(self):
-    #     """
-    #     ### REGRA:
-    #     simple-expression := term \{ additive-op term \}
-    #     """
-
-    #     simple_expression = self.term()
-
-    #     # TODO: fazer checagem de additive-op
-    #     while (self.lookahead.tag == TAGS.ADDITIVE_OP.value):
-    #         pass
-
-    #     return simple_expression
+            return None
 
     def simple_expression(self):
         """
@@ -254,21 +347,27 @@ class Parser:
         sum_simple_expression = self.sum_simple_expression()
         return SimpleExpression(simple_expression, sum_simple_expression)
     
-    # antes de atualizar remover caso necessario apos o pr
-    # def term(self):
-    #     """
-    #     ### REGRA:
-    #     term := factor \{ multiplicative-op factor \}
-    #     """
-    #     term = self.factor()
+    def sum_simple_expression(self):
+        """
+        ### REGRA:
+        <sum-simple-expression> := <additive-op> <term> <sum-simple-expression> | e
+        """
+        if self.lookahead.tag == TAGS.ADDITIVE_OP.value:
+            op = self.lookahead
+            self.match_tag(TAGS.ADDITIVE_OP.value)
 
-    #     # TODO: fazer checagem de multiplicative-op
-    #     while (self.lookahead.tag == TAGS.MULTIPLICATIVE_OP):
-    #         pass
+            term = self.term()
+            next_expr = self.sum_simple_expression()
 
-    #     return term
+            return SumSimpleExpression(op, term, next_expr)
+
+        return None
 
     def term(self):
+        """
+        ### REGRA:
+        term := <factor> <multiplicative-expr>
+        """
         left = self.factor()
         rest = self.multiplicative_expr()
         return Term(left, rest)
@@ -284,67 +383,144 @@ class Parser:
             return MultiplicativeExpr(op, right, next_expr)
 
         return None
-    
-    ##parei por aqui
 
     def factor(self):
         """
         ### REGRA:
-        factor := literal
-                | identifier
-                | function-call
-                | sub-expression
-                | unary
+        <factor> := <literal>
+                 |  <identifier>
+                 |  <sub-expression>
+                 |  <unary>
         """
+
+        # literal
+        if self.lookahead.tag == TAGS.LITERAL.value:
+            token = self.lookahead
+            self.match_tag(TAGS.LITERAL.value)
+            return Literal(token)
+        
         # identifier
         if self.lookahead.tag == TAGS.IDENTIFIER.value:
-            return self.identifier()
+            identifier = self.identifier()
+            return self.factor_tail(identifier)
 
         # sub-expression
         if self.lookahead.tag == ord("("):
-            expr: Expression = self.expression()
+            return self.sub_expression()
+
+        # unary
+        if self.lookahead.tag in (ord("+"), ord("-"), TAGS.NOT.value):
+            return self.unary()
+
+        raise ParseError("Factor inválido")
+
+    def factor_tail(self, identifier):
+        """
+        ### REGRA:
+        <factor_tail> := "(" <actual-params> ")" | e
+        """
+
+        if self.match_tag(ord("(")):
+            params = self.actual_params()
 
             if not self.match_tag(ord(")")):
-                raise ParseError(
-                    f"ERRO NA LINHA {self.get_current_line()}: esperado fechamento de parênteses ')'"
-                )
-            
-            return expr
-        
-        # unary
-        # TODO: refatorar isso
-        if self.lookahead.tag in (ord("+"), ord("-"), ord("not")):
-            pass
+                raise ParseError("Esperado ')'")
+
+            return FunctionCall(identifier, params)
+
+        return identifier
 
     def unary(self):
         """
         ### REGRA:
         unary := ("+" | "-" | "not") { expression }
         """
-        pass
+        if self.lookahead.tag in (ord("+"), ord("-"), TAGS.NOT.value):
+            op = self.lookahead
+            self.match_tag(self.lookahead.tag)
+
+            expr = self.unary_expression()
+
+            return UnaryExpr(op, expr)
+
+        raise ParseError(
+            f"ERRO NA LINHA {self.get_current_line()}: operador unário esperado"
+        )
+    
+    def unary_expression(self):
+        """
+        ### REGRA:
+        <unary-expression> := <expression> <unary-expression> | e
+        """
+        if self.lookahead.tag in (
+            TAGS.IDENTIFIER.value,
+            ord("("),
+            TAGS.LITERAL.value,
+        ):
+            expr = self.expression()
+            next_expr = self.unary_expression()
+            return UnaryExpression(expr, next_expr)
+
+        return None
 
     def sub_expression(self):
         """
         ### REGRA:
         sub-expr := "(" expression ")"
         """
-        pass
 
-    def function_call(self):
-        """
-        ### REGRA:
-        function_call := identifier "(" [actual-params] ")"
-        """
-        pass
+        if not self.match_tag(ord("(")):
+            raise ParseError("Esperado '('")
+        
+        expr = self.expression()
+
+        if not self.match_tag(ord(")")):
+            raise ParseError("Esperado ')'")
+
+        return expr
     
-    def identifier(self) -> Identifier:
+    def actual_params(self):
+        """
         ### REGRA:
-        ## <identifier> := <valid_char> <maybe_valid_char>
-        ###
-        if self.lookahead.tag != TAGS.IDENTIFIER.value:
+        <actual-params> := <expression> <maybe-expression> | e
+        """
+        if self.lookahead.tag in (
+            TAGS.IDENTIFIER.value,
+            ord("("),
+            TAGS.LITERAL.value,
+        ):
+            expr = self.expression()
+            rest = self.maybe_expression()
+            return ActualParams(expr, rest)
+
+        return None
+
+    def maybe_expression(self):
+        """
+        ### REGRA:
+        <maybe-expression> := "," <expression> <maybe-expression> | e
+        """
+        if self.lookahead.tag == ord(","):
+            self.match_tag(ord(","))
+
+            expr = self.expression()
+            rest = self.maybe_expression()
+
+            return MaybeExpression(expr, rest)
+
+        return None
+
+    def identifier(self) -> Identifier:
+        """
+        ### REGRA:
+        <identifier> := <valid_char> <maybe_valid_char>
+        """
+
+        token = self.lookahead
+
+        if not self.match_tag(TAGS.IDENTIFIER.value):
             raise ParseError(
                 f"ERRO NA LINHA {self.get_current_line()}: esperado identificador"
             )
-        token = self.lookahead
-        self.match_tag(TAGS.IDENTIFIER.value)
+
         return Identifier(token)
